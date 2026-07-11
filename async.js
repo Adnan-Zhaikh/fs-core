4c04161eecc39d806a6eb6959c7573beef23ce6d
Adnan-Zhaikh <adnanibrahimshaikh@gmail.com>
2026-07-11 23:19:57 +0530

    Updated

--- async.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const delayMessage = () => {
-    return new Promise(resolve => setTimeout(() => resolve("Data Payload Downloaded!"), 1500));
-};
-const runEngine = async () => {
-    console.log("Initiating request...");
-};
-const response = await delayMessage();
-runEngine();
